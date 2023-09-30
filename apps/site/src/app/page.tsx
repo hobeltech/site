76a3529084ea2d3af78aa996dda35289df53ce55
npclaudiu <npclaudiu@gmail.com>
2023-10-01 00:40:58 +0300

    Add logo to landing page

--- apps/site/src/app/page.tsx
+++ apps/site/src/app/page.tsx
@@ -1,8 +1,15 @@
-import styles from './page.module.css'
+import Image from 'next/image';
+import styles from './page.module.css';
 
 export default function Home() {
   return (
     <main className={styles.main}>
+      <Image
+        src="/logo.png"
+        width={321}
+        height={319}
+        alt="Hobeltech"
+      />
       Under construction
     </main>
   )

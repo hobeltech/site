e06fc5e4dfabf19b595b96df8bc98eb9cd0e0fac
Claudiu Nedelcu <npclaudiu@users.noreply.github.com>
2025-12-06 16:24:32 +0200

    create placeholder landing page

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const interSans = Inter({
+  variable: "--font-inter-sans",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Hobeltech",
+  description: "Hobeltech official website",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${interSans.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
